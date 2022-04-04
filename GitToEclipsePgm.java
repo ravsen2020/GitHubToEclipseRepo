620a47fee7f963e1e5e6e0625a5d69e9877a22d5
ravsen2020 <senthilkumar.venu@gmail.com>
2022-04-03 23:24:54 -0500

    added some comments in the program

--- GitToEclipsePgm.java
+++ GitToEclipsePgm.java
@@ -3,7 +3,7 @@ public class GitToEclipsePgm {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Om muruga,   Om muruga");
-		
+		//added comments for test
 
 	}
 
